67962b8a192ee9d52a634ad93ddb990a424d8fc8
André Brenner <andre.luizrb@hotmail.com>
2025-01-25 04:13:55 -0300

    feat:adding api routes(transactions,balance,login)

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\TransactionsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+   return json_encode(["status" => "online"]);
+});
+
+Route::get('/login', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('transactions')->group(function(){
+    /* Get all transactions */
+    Route::get('/', function (Request $request) {
+        //
+    })->middleware('auth:sanctum');
+
+    /* Get a especific transaction  */
+    Route::get('/{id}', function (Request $request) {
+        //
+    })->middleware('auth:sanctum');
+
+    Route::post('/store', function (Request $request) {
+         //
+    })->middleware('auth:sanctum');
+});
+
+
+Route::get('/get_account_balance', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
