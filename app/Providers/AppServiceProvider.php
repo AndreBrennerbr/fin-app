fb97b41786c76d84f75ea5f4e116d72a7b49ef77
André Brenner <andre.luizrb@hotmail.com>
2025-01-28 18:31:20 -0300

    feat: adding dependency injection by the service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Interfaces\TransactionInterfaceRepository;
+use App\Repositories\Interfaces\UserInterfaceRepository;
+use App\Repositories\TransactionRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserInterfaceRepository::class, UserRepository::class);
+        $this->app->bind(TransactionInterfaceRepository::class, TransactionRepository::class);
     }
 
     /**
