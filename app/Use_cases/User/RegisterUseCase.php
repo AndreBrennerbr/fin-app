0a9d3647d6559586d1f5f9b84b21eca322566aee
André Brenner <andre.luizrb@hotmail.com>
2025-01-28 22:57:57 -0300

    feat:adding Register user case

---
+++ app/Use_cases/User/RegisterUseCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Use_cases\User;
+
+use App\Repositories\Interfaces\UserInterfaceRepository;
+
+
+class RegisterUseCase{
+
+    private UserInterfaceRepository $repository;
+
+    public function __construct(UserInterfaceRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(array $data){
+        return $this->repository->create($data);
+    }
+}
