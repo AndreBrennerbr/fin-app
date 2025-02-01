4553041a2224aa8ee1659fb0e1be4f4e79d1f281
André Brenner <andre.luizrb@hotmail.com>
2025-02-01 01:09:43 -0300

    fix: change type from find to collection

--- app/Repositories/Interfaces/TransactionInterfaceRepository.php
+++ app/Repositories/Interfaces/TransactionInterfaceRepository.php
@@ -2,11 +2,12 @@
 namespace App\Repositories\Interfaces;
 
 use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Collection;
 
 interface TransactionInterfaceRepository
 {
     public function all(): array;
-    public function findById(int $id): ?Transaction;
+    public function findById(int $id): ?Collection;
     public function create(array $data): Transaction;
     public function update(int $id, array $data): bool;
 }
