c96bae9eaa0e4f60972929b51b1e7314f0f469ea
André Brenner <andre.luizrb@hotmail.com>
2025-02-01 01:07:45 -0300

    fix:removing user_id from fillabe! model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    protected $fillable = ['type', 'user_id', 'category', 'value','description','date_created_transaction'];
+    protected $fillable = ['type', 'category', 'value','description','date_created_transaction'];
 
     protected $hidden = [
         'user_id'
