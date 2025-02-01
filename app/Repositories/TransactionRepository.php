a8e7233c13d6f88d8e7a85da84f662ad074b6d8f
André Brenner <andre.luizrb@hotmail.com>
2025-02-01 01:11:27 -0300

    fix: Add protection against IDOR by adding 'user_id' parameter

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -3,23 +3,33 @@ namespace App\Repositories;
 
 use App\Models\Transaction;
 use App\Repositories\Interfaces\TransactionInterfaceRepository;
-
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 class TransactionRepository implements TransactionInterfaceRepository{
+    
+    private $userId;
 
+    public function __construct() {
+        $this->userId = Auth::id();
+    }
+    
     public function all(): array
     {
         try {
-            return Transaction::all()->toArray();
+            return Transaction::where('user_id', $this->userId)->get()->toArray();
         } catch (\Illuminate\Database\QueryException $e) {
             throw new \RuntimeException('Erro ao listar transações', 500);
         }
         
     }
 
-    public function findById(int $id): ?Transaction
+    public function findById(int $id): ?Collection
     {
         try {
-            return Transaction::find($id);
+            $transaction = Transaction::where('user_id',$this->userId)
+                        ->where('id',$id)
+                        ->get();
+            return $transaction;
         } catch (\Illuminate\Database\QueryException $e) {
             throw new \RuntimeException('Erro ao listar a transação', 500);
         }
@@ -29,7 +39,11 @@ class TransactionRepository implements TransactionInterfaceRepository{
     public function create(array $data): Transaction
     {
         try {
-            return Transaction::create($data);
+            $transaction = New Transaction();
+            $transaction->user_id = $this->userId;
+            $transaction->fill($data); 
+            $transaction->save(); 
+            return  $transaction;
         } catch (\Illuminate\Database\QueryException $e) {
             throw new \RuntimeException('Erro ao criar transação', 500);
         }
@@ -39,7 +53,9 @@ class TransactionRepository implements TransactionInterfaceRepository{
     public function update(int $id, array $data): bool
     {
         try {
-            $transaction = Transaction::find($id);
+            $transaction = Transaction::where('user_id', $this->userId)
+                    ->where('id',$id)
+                    ->first();
             if (!$transaction) return false;
             return $transaction->update($data);
         } catch (\Illuminate\Database\QueryException $e) {
